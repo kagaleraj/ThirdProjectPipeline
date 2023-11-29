db87c796c8bb9bdb938ab016bf307cdb3430cd43
kagaleraj <134764142+kagaleraj@users.noreply.github.com>
2023-11-29 16:20:39 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdProjectPipeline
+{
+    internal class Program
+    {
+        public void Mul()
+        {
+            int a, b, c;
+            a = 5;
+            b = 5;
+            c = a * b;
+            Console.WriteLine("Mul=" + c);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Mul();
+            Console.ReadLine();
+        }
+    }
+}
